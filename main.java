8004c8f8601c6e1c1d1a39a951a97bea47d28030
Liam Butler <174329665+liambfbg@users.noreply.github.com>
2025-03-03 17:32:36 +0000

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+package booleanV2;
+
+import booleanV2.BooleanV2
+
+public class Main {
+
+  public static void main(String[] args) {
+    BooleanV2 newBool = BooleanV2.PERHAPS;
+    System.out.println(newBool);
+  }
+
+}
